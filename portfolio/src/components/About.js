9f7053a9b9ea82a866e394070d124959038e6c86
bucholski <bucholskim@gmail.com>
2022-03-27 12:28:21 +0200

    Update details

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -3,13 +3,13 @@ import React from "react";
 const About = () => {
   return (
     <section className="about">
-      <span className="bigger-text">Hi, I'm Michał!</span>
+      <p className="bigger-text">Hi, I'm Michał!</p>
       <p>I am a beginner frontend developer looking for my first job!</p>
       <p>
         I love <b>CSS</b> and know my way around <b>JS</b>.
       </p>
       <p>
-        So far I have made several little projects. You can see them in the{" "}
+        So far I have made several small projects. You can find them in the{" "}
         <a href="#portfolio-section">portfolio</a> section.{" "}
       </p>
     </section>
